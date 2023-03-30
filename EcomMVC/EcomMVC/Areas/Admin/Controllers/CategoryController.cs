9f463002326df9d5086f1c56a5e338f06f091cc4
Manoj Kumar <manoj.kumar@3pillarglobal.com>
2023-03-30 22:27:27 +0530

    Added Admin area and created Category controller with list view

---
+++ EcomMVC/EcomMVC/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using EcomMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcomMVC.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            List<Category> categoryList = new List<Category>();
+            categoryList.Add(new Category() { CategoryId = 1, CategoryName = "Electronics" });
+            categoryList.Add(new Category() { CategoryId = 2, CategoryName = "Home" });
+
+            return View(categoryList);
+        }
+    }
+}
