7ba487ba9e6c50c89207f56238dc80841480d2ca
maheshgrnoidac43 <maheshgrnoidac43@gmail.com>
2023-03-30 20:14:01 +0530

    Product and Category Model has been created.

---
+++ EcomMVC/EcomMVC/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EcomMVC.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        
+        public string ProductName { get; set; }
+    
+        public double UnitPrice { get; set; }
+
+        public string Description { get; set; }
+        
+        public string CategoryId { get; set; }
+    }
+}
