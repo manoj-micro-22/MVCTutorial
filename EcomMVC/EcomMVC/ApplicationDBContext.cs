2768648b6580686fcd5beb70f0312968d16aa67d
Manoj Kumar <manoj.kumar@3pillarglobal.com>
2023-04-04 00:06:39 +0530

    Added entity framework for DB operation

---
+++ EcomMVC/EcomMVC/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using EcomMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EcomMVC
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext() : base("ConstrApplicationDB")
+        {
+
+        }
+
+        public DbSet<Product> products { get; set; }
+        public DbSet<Category> catgories { get; set; }
+    }
+}
